72f27b6f9e2f32f0eb86134bb5d9f494e7ded666
Datguyovrder <rhui@hawk.iit.edu>
2018-06-15 11:36:23 -0500

    current

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
 const morgan = require('morgan');
 const urlEncodedParser = bodyParser.urlencoded({extended: true});
 const PORT = 3000;
 const views = require("./views");
-const { db } = require('./models');
+const {Page, User, db} = require('./models');
 
 db.authenticate().then(()=>{
   console.log('connected to the datbase');
@@ -19,5 +18,10 @@ app.get('/', (req, res, next) => {
   res.send(views.main());
 });
 
+const init= async () => {
+  await db.sync({force: true})
 
-app.listen(PORT, () => {console.log('Listening on Port:' + PORT)});
+  app.listen(PORT, () => {console.log('Listening on Port:' + PORT)});
+}
+
+init()
--- models/index.js
+++ models/index.js
@@ -1,7 +1,31 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack');
+const db = new Sequelize('postgres://localhost:5432/wikistack', {
+  logging: false
+});
 
+const Page = db.define('page', {
+  title: {
+    type: Sequelize.STRING
+  },
+  slug: {
+    type: Sequelize.STRING
+  },
+  content: {
+    type: Sequelize.TEXT
+  },
+  status: {
+    type: Sequelize.ENUM('open', 'closed')
+  }
+});
 
-module.exports = {
-  db
-}
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING
+  }
+});
+
+module.exports = { Page, User, db };
+
